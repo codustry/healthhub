553f5486414d895b43626bb2e9cf95a3fa2a31c8
Nutchanon Ninyawee <nutchanon@codustry.com>
2020-07-21 04:47:10 +0700

    add missing l

--- ios/Classes/HealthhubPlugin.h
+++ ios/Classes/HealthhubPlugin.h
@@ -1,4 +1,4 @@
 #import <Flutter/Flutter.h>
 
-@interface HeathhubPlugin : NSObject<FlutterPlugin>
+@interface HealthhubPlugin : NSObject<FlutterPlugin>
 @end
--- ios/Classes/HealthhubPlugin.m
+++ ios/Classes/HealthhubPlugin.m
@@ -1,15 +1,15 @@
-#import "HeathhubPlugin.h"
+#import "HealthhubPlugin.h"
 #if __has_include(<heathhub/heathhub-Swift.h>)
-#import <heathhub/heathhub-Swift.h>
+#import <healthhub/healthhub-Swift.h>
 #else
 // Support project import fallback if the generated compatibility header
 // is not copied when this plugin is created as a library.
 // https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
-#import "heathhub-Swift.h"
+#import "healthhub-Swift.h"
 #endif
 
-@implementation HeathhubPlugin
+@implementation HealthhubPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftHeathhubPlugin registerWithRegistrar:registrar];
+  [SwiftHealthhubPlugin registerWithRegistrar:registrar];
 }
 @end
